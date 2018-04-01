c4dec12b3c6656092c7735508b447ed602edc2a3
Jonathan Woltz <jwoltz13@gmail.com>
2018-04-01 17:59:24 -0400

    Initial HTML and trivia objects without timers or JQuery

---
+++ assets/javascript/app.js
@@ -0,0 +1,45 @@
+var triviaObject = [
+    {
+        question: "What is the most popular race/class combination in the game?",
+        answerIndex: 2,
+        answers: ["Goblin Rogue", "High-Elf Wizard", "Human Fighter", "Aasimar Paladin"]
+    },
+    {
+        question: "Who was the creator of the original Dungeons & Dragons game?",
+        answerIndex: 3,
+        answers: ["Dan Harmon", "Matthew Mercer", "Jeremy Crawford", "Gary Gaigax"]
+    },
+    {
+        question: "What is the name of the company that publishes and owns the game?",
+        answerIndex: 2,
+        answers: ["Paizo", "Parker Brothers", "Wizards of the Coast", "Hasbro"]
+    },
+    {
+        question: "Which standard two-handed weapon has the highest average damage?",
+        answerIndex: 0,
+        answers: ["Great Sword", "Quarterstaff", "Great Axe", "Maul"]
+    },
+    {
+        question: "What is the most essential mental attribute for a Sorcerer?",
+        answerIndex: 3,
+        answers: ["Sanity", "Intelligence", "Wisdom", "Charisma"]
+    },
+    {
+        question: "What is the common term for a person who argues excessively over rules in Dungons & Dragons?",
+        answerIndex: 1,
+        answers: ["Rules Ninny", "Rules Lawyer", "Rule Tool", "Armstrong"]
+    },
+    {
+        question: "What is the latest edition of Dungeons & Dragons?",
+        answerIndex: 0,
+        answers: ["5e", "3.5", "6e", "AD&D"]
+    },
+    {
+        question: "Which class gets the 'Sneak Attack' feature?",
+        answerIndex: 1,
+        answers: ["Fighter", "Rogue", "Paladin", "Ranger"]
+    }
+]
+
+var gameStarted = false;
+
